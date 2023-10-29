9a9d22cdf7433eb5c34bb8ba06e8afb6783f610c
Iker <143113054+Ikerdiazmontilla@users.noreply.github.com>
2023-10-28 19:17:35 -0500

    Display content in calculator all buttons work
    It doesn't operate yet but it displays the content
    well.
    
    Bugs to solve:
    1. When you have pressed the first number and then
    the operator you have to be able to change the
    operator by pressing another operation button.
    But if you have pressed the second number when
    you press the operator the outcome should be the
    result plus the operator in the upper part
    of the panel and the result in the lower part.
    
    2. The = button only should be actionable when
    the user has pressed a first number, an operator
    and a second number.

--- script.js
+++ script.js
@@ -33,4 +33,85 @@ function operate(a, operator, b){
     }
 }
 
-console.log(operate(10, "-", 3))
+const divOperation = document.querySelector(".operation")
+const divSolution = document.querySelector(".solution")
+
+
+
+let solutionValue = 0
+let operationValue = 0
+
+function hasOperator(){
+    if(divOperation.textContent[divOperation.textContent.length - 1] === "×" || divOperation.textContent[divOperation.textContent.length - 1] === "+"|| divOperation.textContent[divOperation.textContent.length - 1] === "-"|| divOperation.textContent[divOperation.textContent.length - 1] === "÷"){
+        return true
+    }
+    return false
+}
+
+function print(event){
+    if(hasOperator()){
+
+        divSolution.textContent = event.target.textContent
+    }
+    else{
+        if(divSolution.textContent == 0)
+            divSolution.textContent = event.target.textContent
+        else{
+            divSolution.textContent += event.target.textContent
+        }
+    } 
+    solutionValue = divSolution.textContent
+}
+
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach(number => {
+    number.addEventListener("click", print)
+});
+
+
+function printOperator(event){
+    divOperation.textContent = `${solutionValue} ${event.target.textContent}`
+    operationValue = divOperation.textContent
+    divSolution.textContent = parseInt(divSolution.textContent)
+}
+
+
+const operators = document.querySelectorAll(".operator")
+operators.forEach(operator => {
+    operator.addEventListener("click", printOperator)
+});
+
+
+
+
+function printEqual(event){
+    if(hasOperator() && divSolution.textContent){
+    divOperation.textContent += ` ${divSolution.textContent} ${event.target.textContent}`
+    divSolution.textContent = solution}
+}
+
+
+
+const equal = document.querySelector(".equal")
+equal.addEventListener("click", printEqual)
+
+
+
+
+const clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+    divOperation.textContent = ""
+    divSolution.textContent = "0"
+})
+
+const del = document.querySelector(".delete")
+del.addEventListener("click", () => {
+    if(divSolution.textContent.length > 1){
+        divSolution.textContent = divSolution.textContent.slice(0, divSolution.textContent.length - 1)
+    }
+    else{
+        divSolution.textContent = 0
+    }
+})
+
